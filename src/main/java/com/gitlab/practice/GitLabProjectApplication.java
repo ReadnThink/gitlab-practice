6e21d15288840b7c0dcb7b209752d64f11b167a2
solbae <sol4854@naver.com>
2022-12-08 14:31:43 +0900

    first commit

---
+++ src/main/java/com/gitlab/practice/GitLabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLabProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlab/practice/GitLabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
